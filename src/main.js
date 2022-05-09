3453be491ec06118a59b30fb40c41ea5195a1157
zhuaq <zhuaq@7000mall.com>
2022-05-09 14:11:42 +0800

    去除无用测试用例

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Description: xingp，yyds
  * @Author: zaq
  * @Date: 2022-05-09 09:56:54
- * @LastEditTime: 2022-05-09 11:28:33
+ * @LastEditTime: 2022-05-09 14:11:27
  * @LastEditors: zaq
  * @Reference: 
  */
@@ -12,8 +12,6 @@ const bodyParser = require('koa-bodyparser');
 const static = require('koa-static');
 const path = require('path');
 const router = require('./router');
-const { checkDataBase } = require('./util/initialization');
-checkDataBase(12)
 
 const app = new Koa();
 app.use(cors()).use(bodyParser()).use(static(path.join(__dirname, 'public')));
